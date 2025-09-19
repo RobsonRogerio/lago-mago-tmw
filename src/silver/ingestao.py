174d2eca532c01121820ffc65a85f4b6dc4a5232
robson.rogerio@gmail.com <robson.rogerio@gmail.com>
2025-09-19 18:01:41 +0000

    feat: inclusão tabela produtos

--- src/silver/ingestao.py
+++ src/silver/ingestao.py
@@ -3,7 +3,7 @@ def import_query(path):
     with open(path, "r") as open_file:
         return open_file.read()
     
-tablename = 'transacao_produto'
+tablename = dbutils.widgets.get('tablename')
 
 query = import_query(f'{tablename}.sql')
 
