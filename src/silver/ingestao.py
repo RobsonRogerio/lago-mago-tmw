5695749e2615c102cf36ab9fdbd2d5026b21b265
robson.rogerio@gmail.com <robson.rogerio@gmail.com>
2025-09-21 21:24:12 +0000

    feat: inicio do upsert em silver

--- src/silver/ingestao.py
+++ src/silver/ingestao.py
@@ -1,4 +1,8 @@
 # Databricks notebook source
+from delta.tables import delta
+
+# COMMAND ----------
+
 def import_query(path):
     with open(path, "r") as open_file:
         return open_file.read()
@@ -13,3 +17,30 @@ spark.sql(query) \
     .mode('overwrite') \
     .option('overwriteSchema', 'true') \
     .saveAsTable(f'silver.upsell.{tablename}')
+
+# COMMAND ----------
+
+df = spark.readStream \
+        .format('delta') \
+        .option('readChangeFeed', 'true') \
+        .table(f'bronze.upsell.customers')
+
+# COMMAND ----------
+
+silver = delta.table(f'silver.upsell.{tablename}')
+
+def upsert(df, deltatable):
+    query_last = """
+        SELECT *
+        FROM {df}
+        WHERE _change_type <> 'update_preimage'
+        QUALIFY ROW_NUMBER() OVER (PARTITION BY {id} ORDER BY _commit_timestamp DESC)
+        """
+    df_last = spark.sql(query_last, df=df, id='idCliente')
+    
+    pass
+
+stream = df.writeStream \
+            .option("checkpointLocation", f"/Volumes/raw/upsell/cdc/silver_{tablename}_checkpoints") \
+            .foreachBatch(lambda df, epoch: df.write.format('delta').mode('append').saveAsTable(f'silver.upsell.{tablename}')) \
+            .start()
